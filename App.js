4703003dfd545e8b65114bd29d50baf864ad8c28
Arghya <arry@Arghyas-MacBook-Pro.local>
2024-03-26 16:51:47 +0530

    added ui,resturant basic structurer

--- App.js
+++ App.js
@@ -1,26 +1,80 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title1",
-  },
-  "Heading 1"
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://previews.123rf.com/images/shmakova/shmakova2206/shmakova220600109/188025742-grill-menu-hand-drawn-inscription-slogan-food-court-logo-menu-restaurant-bar-cafe-vector-steak-on.jpg"
+    />
+  </a>
 );
-const heading2 = React.createElement(
-  "h1",
-  {
-    id: "title2",
-  },
-  "Heading 2"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const pizzaHut = {
+  name: "Pizza Hut",
+  image:
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7",
+  cusines: ["Pizzas", "monacos"],
+  rating: "4.2",
+};
+const RestaurantCard = () => {
+  return (
+    <div className="card">
+      <img src={pizzaHut.image}></img>
+      <h2>{pizzaHut.name}</h2>
+      <h3>{pizzaHut.cusines.join(", ")}</h3>
+      <h4>{pizzaHut.rating} Star</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <>
+      <div className="restaurant-list">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </>
+  );
+};
+const Footer = () => (
+  <>
+    <h2>Footer</h2>
+  </>
 );
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading1, heading2]
-);
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<AppLayout />);
