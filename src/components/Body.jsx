1994778a3dddbd645337f389309751922de3db6b
Arghya Jana <arghya0017@gmail.com>
2024-04-03 01:00:20 +0530

    taken swiggy public agi and implimented this

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { restaurantList } from "../constants.jsx";
 import RestaurantCard from "./RestaurantCard.jsx";
 
@@ -6,12 +6,38 @@ filterData = (searchText, restaurants) => {
   const filterData = restaurants.filter((restaurant) =>
     restaurant.info.name.includes(searchText)
   );
+
   return filterData;
 };
 
+const getXRestaurants = (json) => {
+  const filterResturant = json.data.cards.filter((item) => {
+    return (
+      item?.card?.card?.gridElements?.infoWithStyle["@type"] ===
+      "type.googleapis.com/swiggy.presentation.food.v2.FavouriteRestaurantInfoWithStyle"
+    );
+  });
+
+  return filterResturant;
+};
 const Body = () => {
   const [restaurants, setRestaurants] = useState(restaurantList);
   const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+  getRestaurants = async () => {
+    let data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    const restaurants = await getXRestaurants(json);
+    setRestaurants(
+      restaurants[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+    
+  };
+
   return (
     <>
       <div className="search-container">
@@ -25,7 +51,6 @@ const Body = () => {
         <button
           className="seacrch-btn"
           onClick={() => {
-            
             const data = filterData(searchText, restaurants);
             setRestaurants(data);
           }}
